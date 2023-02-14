d9b50bbca265be3ba06d293e386c7f6d67e899e2
Srz97 <shahraiz97@gmail.com>
2023-02-14 14:57:45 +0000

    update changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
-$(document).ready(function(){
 
-    $(".btn--Rock" ).click(function() {
-        $(".btn--Paper").hide();
-        $(".btn--Scissors").hide();
-    });
+
+
+$(document).ready(function(){
+    $(".computer-area .btn").hide();
+    $( ".player-area  .btn--click" ).click(function() {
+        $(".player-area .btn--click").hide();
+        $(this).show();
+
+        var check_cliked_option = $(this).attr("value");
+               
+        var rendom_int = Math.floor((Math.random() * 3) + 1);
+        
+        $(".computer-area .btn--game-"+rendom_int).show();
+
+        var result = "";
+
+        if(check_cliked_option == rendom_int){
+            result = "Draw";
+        }
+        if(check_cliked_option > rendom_int){
+            result = "Player wins";
+        }
+        if(check_cliked_option < rendom_int){
+            result = "Computer wins";
+        }
+        $(".result-area").html(result);
+
+        
+      });
 });

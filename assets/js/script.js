f141d58162b2e3d7c33bf9c0a54d094394339454
Srz97 <shahraiz97@gmail.com>
2023-02-14 17:59:25 +0000

    update changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,34 +4,48 @@
 $(document).ready(function(){
 
     $(".computer-area .btn").hide();
-    $( ".player-area  .btn--click" ).click(function() {
-        $(".player-area .btn--click").hide();
-        $(this).show();
+    $( ".player-area.active  .btn--click" ).click(function() {
+        //checking condition either the game is active or not 
+        if($(".player-area").hasClass("active") == true){
+            //if the game is active then run the below code 
+            $(".player-area .btn--click").hide();
+            $(this).show();
 
-        var check_cliked_option = $(this).attr("value");
-               
-        var rendom_int = Math.floor((Math.random() * 3) + 1);
-        
-        $(".computer-area .btn--game-"+rendom_int).show();
+            var check_cliked_option = $(this).attr("value");
+                
+            var rendom_int = Math.floor((Math.random() * 3) + 1);
+            
+            $(".computer-area .btn--game-"+rendom_int).show();
 
-        var result = "";
+            var result = "";
 
-        if(check_cliked_option == rendom_int){
-            result = "Draw";
+            if(check_cliked_option == rendom_int){
+                result = "Draw";
+                var current_ti_score = parseInt($(".draw span").html());
+                current_ti_score = current_ti_score + 1;
+                $(".draw span").html(current_ti_score)
+            }
+            if(check_cliked_option > rendom_int){
+                result = "You won!";
+                var current_wi_score = parseInt($(".your-score span").html());
+                current_wi_score = current_wi_score + 1;
+                $(".your-score span").html(current_wi_score)
+            }
+            if(check_cliked_option < rendom_int){
+                result = "Computer wins";
+            }
+            $(".result-area").html(result);
+            // remove the active class once game is finish 
+            $(".player-area").removeClass("active");
         }
-        if(check_cliked_option > rendom_int){
-            result = "You win!";
-        }
-        if(check_cliked_option < rendom_int){
-            result = "Computer wins";
-        }
-        $(".result-area").html(result);
 
         
       });
       $( "#reset-btn" ).click(function() {
         $(".computer-area .btn").hide();
         $(".player-area .btn--click").show();
+        // activate game again
+        $(".player-area").addClass("active");
       });
 });
 

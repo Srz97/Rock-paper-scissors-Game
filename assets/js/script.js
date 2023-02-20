e0c3f51196b172be92e5deeb8fa9c0b99e174aea
Srz97 <shahraiz97@gmail.com>
2023-02-20 22:18:15 +0000

    update changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,8 +51,30 @@ $(document).ready(function(){
         $(".player-area").addClass("active");
         $(".result-area").html("Choose your Warrior")
       });
-      $( "#rules-btn" ).click(function(){
-         alert("Rock wins against scissors; paper wins against rock; and scissors wins against paper. If both players throw the same hand signal, it is considered a tie.")
-      });
+     
 });
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("rules-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}

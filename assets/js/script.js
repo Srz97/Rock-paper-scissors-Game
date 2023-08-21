9bc8c8125c4f8b0daea32b6a8ad647836a52faf8
Srz97 <shahraiz97@gmail.com>
2023-08-21 11:00:39 +0000

    update the reset button settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("rules-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 $(document).ready(function(){
 
   $(".computer-area .btn").hide();
@@ -49,45 +76,19 @@ $(document).ready(function(){
           // Display result and remove the active class
           $(".result-area").html(result);
           $(".player-area").removeClass("active");
-      }
+      
+          // Set a timer to reset the game after 2 seconds (adjust the time as needed)
+          setTimeout(function() {
+            resetGame();
+        }, 2000);
+        }
+      });
 
       
+      function resetGame() {
+        $(".computer-area .btn").hide();
+        $(".player-area .btn--click").show();
+        $(".player-area").addClass("active");
+        $(".result-area").html("Choose your Warrior");
+    }
   });
-
-  $( "#reset-btn" ).click(function() {
-      // Hide computer buttons and show player buttons
-      $(".computer-area .btn").hide();
-      $(".player-area .btn--click").show();
-      // Activate the game
-      $(".player-area").addClass("active");
-      // Reset result area message
-      $(".result-area").html("Choose your Warrior");
-  });
-   
-});
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("rules-btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}

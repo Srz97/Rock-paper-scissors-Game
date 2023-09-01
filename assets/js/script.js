4b679010262da80e015dc5d57f3aa728c8e55e81
Srz97 <shahraiz97@gmail.com>
2023-09-01 22:44:02 +0000

    add the score in the score area while playing

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,68 +25,79 @@ window.onclick = function(event) {
 }
 
 
-$(document).ready(function(){
-  // Initially, hide all buttons in both player and computer areas
-  $(".game-area .btn--click").hide();
+$(document).ready(function () {
+  // Initialize scores
+  var yourScore = 0;
+  var computerScore = 0;
+  var drawScore = 0;
+
+  // Hide computer choice buttons initially
+  $(".computer-area .btn").hide();
 
   // Function to determine the winner and update the scores
   function determineWinner(playerChoice, computerChoice) {
-      // Rock: 2, Paper: 3, Scissors: 1
-      if (playerChoice === computerChoice) {
-          return "Oh, it's a draw!";
-      } else if (
-          (playerChoice === 2 && computerChoice === 1) ||
-          (playerChoice === 3 && computerChoice === 2) ||
-          (playerChoice === 1 && computerChoice === 3)
-      ) {
-          return "You win!";
-      } else {
-          return "Your device wins!";
-      }
+    // Rock: 2, Paper: 3, Scissors: 1
+    if (playerChoice === computerChoice) {
+      drawScore++;
+      $(".draw span").html(drawScore);
+      return "It's a draw!";
+    } else if (
+      (playerChoice === 2 && computerChoice === 1) ||
+      (playerChoice === 3 && computerChoice === 2) ||
+      (playerChoice === 1 && computerChoice === 3)
+    ) {
+      yourScore++;
+      $(".your-score span").html(yourScore);
+      return "You win!";
+    } else {
+      computerScore++;
+      $(".computers-score span").html(computerScore);
+      return "Computer wins!";
+    }
   }
 
   // Function to reset the game
   function resetGame() {
-      // Hide all buttons in both player and computer areas
-      $(".game-area .btn--click").hide();
+    // Hide all buttons in both player and computer areas
+    $(".game-area .btn--click").hide();
 
-      // Show player buttons and make player area active
-      $(".player-area .btn--click").show();
-      $(".player-area").addClass("active");
+    // Show player buttons and make player area active
+    $(".player-area .btn--click").show();
+    $(".player-area").addClass("active");
 
-      // Clear the result area
-      $(".result-area").html("Choose your warrior");
+    // Clear the result area
+    $(".result-area").html("Choose your warrior");
   }
 
   // Event handler for player's button click
-  $(".player-area.active .btn--click").click(function() {
-      // Checking if the game is active
-      if ($(".player-area").hasClass("active")) {
-          // Get the value of the clicked option (1, 2, or 3)
-          var playerChoice = parseInt($(this).attr("value"));
+  $(".player-area.active .btn--click").click(function () {
+    // Checking if the game is active
+    if ($(".player-area").hasClass("active")) {
+      // Get the value of the clicked option (1, 2, or 3)
+      var playerChoice = parseInt($(this).attr("value"));
 
-          // Generate a random integer between 1 and 3 for the computer's choice
-          var computerChoice = Math.floor(Math.random() * 3) + 1;
+      // Generate a random integer between 1 and 3 for the computer's choice
+      var computerChoice = Math.floor(Math.random() * 3) + 1;
 
-          // Hide all buttons in both player and computer areas
-          $(".game-area .btn--click").hide();
+      // Hide all buttons in both player and computer areas
+      $(".game-area .btn--click").hide();
 
-          // Show only the clicked button and the computer's choice button
-          $(this).show();
-          $(".computer-area .btn--click[value='" + computerChoice + "']").show();
+      // Show only the clicked button and the computer's choice button
+      $(this).show();
+      $(".computer-area .btn--click[value='" + computerChoice + "']").show();
 
-          // Determine the winner and update the result area
-          var result = determineWinner(playerChoice, computerChoice);
-          $(".result-area").html(result);
+      // Determine the winner and update the result area
+      var result = determineWinner(playerChoice, computerChoice);
+      $(".result-area").html(result);
 
-          // Remove the active class from the player's area
-          $(".player-area").removeClass("active");
+      // Remove the active class from the player's area
+      $(".player-area").removeClass("active");
 
-          // Set a timer to reset the game after 2 seconds (adjust the time as needed)
-          setTimeout(function() {
-              resetGame();
-          }, 2000);
-      }
+      // Set a timer to reset the game after 2 seconds (adjust the time as needed)
+      setTimeout(function () {
+        resetGame();
+      }, 2000);
+    }
   });
 
   // Initial game setup
